ab9b50dc23bedf627a8fa5fb2de086c2606a8863
CS7player <144120363+CS7player@users.noreply.github.com>
2024-11-28 21:40:12 +0530

    changes

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-app.use(cors());
-app.use(express.json());
 require('./app/utils/config'); // to initallize the global variable in config file from the env file
 require('./app/utils/constants');
+app.use((req, res, next) => {
+    console.log('Origin:', req.get('Origin')); 
+    next();
+  });
+const corsOptions = {
+    origin: (origin, callback) => {
+      if (!origin || ALLOW_ORIGNS.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  };
+  app.use(cors(corsOptions));
+app.use(express.json());
 const {connection} = require('./app/utils/db');
 connection(); // to check the database connecion 
 const routes = require('./app/routes')
--- app/utils/config.js
+++ app/utils/config.js
@@ -6,3 +6,4 @@ global.MONGO_DB_PASSWORD = process.env.MONGO_DB_PASSWORD;
 global.MONGO_DB_HOST = process.env.MONGO_DB_HOST;
 global.MONGO_DB_COLLECTION = process.env.MONGO_DB_COLLECTION;
 global.SECRET_KEY = process.env.SECRET_KEY;
+global.ALLOW_ORIGNS = process.env.ALLOW_ORIGNS;
